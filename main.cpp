b79673560361162e5ce180e00334465b4aff5186
220030cs10 <113435546+220030cs10@users.noreply.github.com>
2023-02-22 13:27:03 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Problem-4
+//
+//  Created by Mahmudjon Obidov on 22/02/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << " We will find circle: " << endl;
+    cin >> a;
+    int radius;
+    cout << " Write your radius: " << endl;
+    cin >> radius;
+    int area = 2 * radius * radius;
+    cout << " Circle is : " << area << endl;
+    
+    return 0;
+}
